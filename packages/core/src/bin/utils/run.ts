eb9d0c80c3c854f8e631ef42078cac40590868d7
hazelnutcloud <hsnmkls@proton.me>
2025-07-07 14:28:11 +0800

    Merge changes from main

--- packages/core/src/bin/utils/run.ts
+++ packages/core/src/bin/utils/run.ts
@@ -609,11 +609,7 @@ export async function run({
 
           if (result.status === "error") onReloadableError(result.error);
 
-          await Promise.all(
-            tables.map((table) =>
-              commitBlock(database.userQB, { table, checkpoint }),
-            ),
-          );
+          await database.commitBlock({ checkpoint, db: database.qb.drizzle });
 
           common.logger.info({
             service: "app",
@@ -635,10 +631,11 @@ export async function run({
           }
         }
 
-        if (event.checkpoints.length > 0) {
-          await database
-            .adminQB("update_checkpoints")
-            .insert(PONDER_CHECKPOINT)
+        await database.wrap({ method: "setCheckpoints" }, async () => {
+          if (event.checkpoints.length === 0) return;
+
+          await database.qb.drizzle
+            .insert(database.PONDER_CHECKPOINT)
             .values(
               event.checkpoints.map(({ chainId, checkpoint }) => ({
                 chainName: indexingBuild.chains.find(
@@ -650,10 +647,12 @@ export async function run({
               })),
             )
             .onConflictDoUpdate({
-              target: PONDER_CHECKPOINT.chainName,
-              set: { latestCheckpoint: sql`excluded.latest_checkpoint` },
+              target: database.PONDER_CHECKPOINT.chainName,
+              set: {
+                latestCheckpoint: sql`excluded.latest_checkpoint`,
+              },
             });
-        }
+        });
 
         break;
       }
